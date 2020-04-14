d66ad7857f1a384bd8cac2f0965888125b5f939c
bcavinee <51139252+bcavinee@users.noreply.github.com>
2020-04-13 21:11:09 -0400

    Updated Settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '48$pm=iqelw3r6yd&*j60s)c-yz^nc@7#5x)m4hfpi!do7on-1'
+#SECRET_KEY = '48$pm=iqelw3r6yd&*j60s)c-yz^nc@7#5x)m4hfpi!do7on-1'
+SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['laboratoryinventorytracker.herokuapp.com']
 
 
 # Application definition
@@ -131,8 +133,8 @@ CRISPY_TEMPLATE_PACK= 'bootstrap4'
 LOGIN_REDIRECT_URL = 'inventory-home'
 
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER= 'inventoryapp248@gmail.com'
+EMAIL_HOST_USER= os.environ.get('EMAIL_USER')
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
-EMAIL_HOST_PASSWORD= "83nascar"
+EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_PASSWORD')
