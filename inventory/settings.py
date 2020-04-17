c699151c1829f8197332a727bf072e408370c2ca
bcavinee <51139252+bcavinee@users.noreply.github.com>
2020-04-16 21:13:34 -0400

    Gmail

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
 
 
 
@@ -139,6 +139,7 @@ EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_HOST_USER= os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT= 587
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 
 django_heroku.settings(locals())
 
